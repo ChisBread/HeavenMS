ac7ecbad29ec6ce338f5d52913b0198804c90bd7
ChisBread <chisbread@gmail.com>
2023-02-02 15:09:26 +0000

    汉化goto命令

--- src/constants/game/GameConstants.java
+++ src/constants/game/GameConstants.java
@@ -62,6 +62,45 @@ public class GameConstants {
     
     // "goto" command for players
     public static final HashMap<String, Integer> GOTO_TOWNS = new HashMap<String, Integer>() {{
+        put("南港", 60000);
+        put("彩虹村", 1000000);
+        put("射手村", 100000000);
+        put("魔法密林", 101000000);
+        put("勇士部落", 102000000);
+        put("废弃都市", 103000000);
+        put("明珠港", 104000000);
+        put("林中之城", 105040300);
+        put("黄金海滩", 110000000);
+        put("诺特勒斯", 120000000);
+        put("圣地", 130000000);
+        put("里恩", 140000000);
+        put("orbis", 200000000);
+        put("happy", 209000000);
+        put("elnath", 211000000);
+        put("ludi", 220000000);
+        put("aqua", 230000000);
+        put("leafre", 240000000);
+        put("mulung", 250000000);
+        put("herb", 251000000);
+        put("omega", 221000000);
+        put("korean", 222000000);
+        put("ellin", 300000000);
+        put("nlc", 600000000);
+        put("showa", 801000000);
+        put("shrine", 800000000);
+        put("ariant", 260000000);
+        put("magatia", 261000000);
+        put("singapore", 540000000);
+        put("quay", 541000000);
+        put("kampung", 551000000);
+        put("amoria", 680000000);
+        put("temple", 270000100);
+        put("square", 103040000);
+        put("neo", 240070000);
+        put("mushking", 106020000);
+    }};
+    
+    public static final HashMap<String, Integer> GOTO_TOWNS_EN = new HashMap<String, Integer>() {{
         put("southperry", 60000);
         put("amherst", 1000000);
         put("henesys", 100000000);
@@ -100,6 +139,7 @@ public class GameConstants {
         put("mushking", 106020000);
     }};
     
+
     // "goto" command for only-GMs
     public static final HashMap<String, Integer> GOTO_AREAS = new HashMap<String, Integer>() {{
         put("gmmap", 180000000);
